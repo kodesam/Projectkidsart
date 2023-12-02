f4705d47ce4e6f341085b6dbd207c423bcfef3ee
Syed Aamir <81068983+kodesam@users.noreply.github.com>
2023-12-02 02:55:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,18 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/upload", methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        f = request.files['artwork']
+        filename = secure_filename(f.filename)
+        # Save the uploaded file to the UPLOAD_FOLDER
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('upload.html')
+
+@app.route("/gallery")
+def gallery():
+    folder_path = app.config['UPLOAD_FOLDER']
+    artworks = [os.path.join(folder_path, filename) for filename in os.listdir(folder_path)]
+    return render_template('gallery.html', artworks=artworks)
